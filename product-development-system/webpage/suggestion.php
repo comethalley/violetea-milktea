664e61f2d649e2c374401275f845c879c227f83d
MJ <mjcariso3@gmail.com>
2023-03-28 20:28:45 +0800

    archive alert

--- product-development-system/webpage/suggestion.php
+++ product-development-system/webpage/suggestion.php
@@ -62,24 +62,24 @@ if ($_SESSION['username']) {
             <div class="cont-left">
                 <nav>
                     <ul class="">
-                    <li class="">
-                        <i class="fa-solid fa-clipboard" style="color: #b8b8b8;"></i> <a class="" href="suggestion.php">Suggestion <span class="sr-only">(current)</span></a>
+                        <li class="">
+                            <i class="fa-solid fa-clipboard" style="color: #b8b8b8;"></i> <a class="" href="suggestion.php">Suggestion <span class="sr-only">(current)</span></a>
                         </li>
                         <div class="line"></div>
                         <li class="">
-                        <i class="fa-solid fa-flask" style="color: #b8b8b8;"></i> <a class="" href="ingredient.php">Ingredients</a>
+                            <i class="fa-solid fa-flask" style="color: #b8b8b8;"></i> <a class="" href="ingredient.php">Ingredients</a>
                         </li>
                         <div class="line"></div>
                         <li class="">
-                        <i class="fa-brands fa-product-hunt" style="color: #b8b8b8;"></i> <a class="" href="product-concept.php">Concept Products</a>
+                            <i class="fa-brands fa-product-hunt" style="color: #b8b8b8;"></i> <a class="" href="product-concept.php">Concept Products</a>
                         </li>
                         <div class="line"></div>
                         <li class="">
-                        <i class="fa-solid fa-chart-simple" style="color: #b8b8b8;"></i> <a class="" href="product-concept.php">Analysis Report</a>
+                            <i class="fa-solid fa-chart-simple" style="color: #b8b8b8;"></i> <a class="" href="product-concept.php">Analysis Report</a>
                         </li>
                         <div class="line"></div>
                         <li class="">
-                        <i class="fa-solid fa-box-archive" style="color: #b8b8b8;"></i> <a class="" data-toggle="collapse" href="#multiCollapseExample1" aria-expanded="false" aria-controls="multiCollapseExample1">
+                            <i class="fa-solid fa-box-archive" style="color: #b8b8b8;"></i> <a class="" data-toggle="collapse" href="#multiCollapseExample1" aria-expanded="false" aria-controls="multiCollapseExample1">
                                 Archive
 
 
@@ -200,7 +200,7 @@ if ($_SESSION['username']) {
                                 <div class="card">
                                     <div class="card-body">
                                         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#studentaddmodal">
-                                        <i class="fa-solid fa-plus" style="color: #ffffff;"></i> Add Research
+                                            <i class="fa-solid fa-plus" style="color: #ffffff;"></i> Add Research
                                         </button>
                                     </div>
                                 </div>
@@ -432,7 +432,7 @@ if ($_SESSION['username']) {
                         </button>
                     </div>
 
-                    <form action="../webpage/includes/archive-user-function.php" method="POST">
+                    <form id="mySuggestion" method="POST">
 
                         <div class="modal-body">
 
@@ -440,22 +440,22 @@ if ($_SESSION['username']) {
                             <h4> Do you want to archive this data?</h4>
                             <div class="form-group">
                                 <label> Username </label>
-                                <input type="text" name="username" id="username" class="form-control" placeholder="Username">
+                                <input type="text" name="username" id="username" class="form-control" placeholder="Username" disabled>
                             </div>
 
                             <div class="form-group">
                                 <label> Subject </label>
-                                <input type="text" name="subject" id="subject" class="form-control" placeholder="Subject">
+                                <input type="text" name="subject" id="subject" class="form-control" placeholder="Subject" disabled>
                             </div>
 
                             <div class="form-group">
                                 <label> Body </label>
-                                <input type="text" name="subject" id="body" class="form-control" placeholder="Body">
+                                <input type="text" name="subject" id="body" class="form-control" placeholder="Body" disabled>
                             </div>
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-dismiss="modal">NO</button>
-                            <button type="submit" name="updatedata" class="btn btn-primary " >YES</button>
+                            <button type="submit" name="updatedata" class="btn btn-primary ">YES</button>
                         </div>
                     </form>
 
@@ -474,7 +474,7 @@ if ($_SESSION['username']) {
                         </button>
                     </div>
 
-                    <form action="../webpage/includes/archive-function.php" method="POST">
+                    <form id="myResearch" method="POST">
 
                         <div class="modal-body">
 
@@ -482,17 +482,17 @@ if ($_SESSION['username']) {
                             <h4> Do you want to archive this data?</h4>
                             <div class="form-group">
                                 <label> Title </label>
-                                <input type="text" name="archive_title" id="archive_title" class="form-control" placeholder="Enter Title">
+                                <input type="text" name="archive_title" id="archive_title" class="form-control" placeholder="Enter Title" disabled>
                             </div>
 
                             <div class="form-group">
                                 <label> Introduction </label>
-                                <input type="text" name="archive_introduction" id="archive_introduction" class="form-control" placeholder="Enter Introduction">
+                                <input type="text" name="archive_introduction" id="archive_introduction" class="form-control" placeholder="Enter Introduction" disabled>
                             </div>
 
                             <div class="form-group">
                                 <label>Trends</label>
-                                <select id="trends" name="archive_trends" id="archive_trend">
+                                <select id="trends" name="archive_trends" id="archive_trend" disbaled>
                                     <option value="social_media">Social Media</option>
                                     <option value="online_forum">Online Forum</option>
                                     <option value="customer_survey">Customer Survey</option>
@@ -501,7 +501,7 @@ if ($_SESSION['username']) {
 
                             <div class="form-group">
                                 <label> Conclusion </label>
-                                <input type="text" name="archive_conclusion" id="archive_conclusion" class="form-control" placeholder="Enter Phone Number">
+                                <input type="text" name="archive_conclusion" id="archive_conclusion" class="form-control" placeholder="Enter Phone Number" disabled>
                             </div>
                         </div>
                         <div class="modal-footer">
@@ -525,7 +525,66 @@ if ($_SESSION['username']) {
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.18/js/dataTables.bootstrap4.min.js"></script>
-
+    <!-- Resarch Archive -->
+    <script>
+        $(document).ready(function() {
+            $('#myResearch').submit(function(event) {
+                event.preventDefault(); // Prevent the form from submitting normally
+                var form = $(this);
+                var url = form.attr('action');
+                $.ajax({
+                    type: "POST",
+                    url: "../webpage/includes/archive-function.php ",
+                    data: form.serialize(), // Serialize the form data
+                    success: function(data) {
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Successfully Archived',
+                            didClose: function() {
+                                // Refresh the page
+                                location.reload();
+                            }
+                        });
+                        console.log(data);
+                    },
+                    error: function(jqXHR, textStatus, errorThrown) {
+                        // Handle any errors that occur during the AJAX request
+                        console.log("Error: ");
+                    }
+                });
+            });
+        });
+    </script>
+    <!-- Suggestion Archive -->
+    <script>
+        $(document).ready(function() {
+            $('#mySuggestion').submit(function(event) {
+                event.preventDefault(); // Prevent the form from submitting normally
+                var form = $(this);
+                var url = form.attr('action');
+                $.ajax({
+                    type: "POST",
+                    url: "../webpage/includes/archive-user-function.php",
+                    data: form.serialize(), // Serialize the form data
+                    success: function(data) {
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Successfully Archived',
+                            didClose: function() {
+                                // Refresh the page
+                                location.reload();
+                            }
+                        });
+                        console.log(data);
+                    },
+                    error: function(jqXHR, textStatus, errorThrown) {
+                        // Handle any errors that occur during the AJAX request
+                        console.log("Error: ");
+                    }
+                });
+            });
+        });
+    </script>
     <script>
         $(document).ready(function() {
 
@@ -566,126 +625,122 @@ if ($_SESSION['username']) {
     </script>
 
     <script>
-       $(document).ready(function() {
+        $(document).ready(function() {
 
-var table = $('#researchtableid').DataTable({
-    "pagingType": "full_numbers",
-    
-    "lengthMenu": [
-        [10, 25, 50, -1],
-        [10, 25, 50, "All"]
-    ],
-    responsive: true,
-    language: {
-        search: "_INPUT_",
-        searchPlaceholder: "Search Your Data",
-    }
-});
+            var table = $('#researchtableid').DataTable({
+                "pagingType": "full_numbers",
 
- 
-});
+                "lengthMenu": [
+                    [10, 25, 50, -1],
+                    [10, 25, 50, "All"]
+                ],
+                responsive: true,
+                language: {
+                    search: "_INPUT_",
+                    searchPlaceholder: "Search Your Data",
+                }
+            });
 
+
+        });
     </script>
 
     <!--Edit research-->
     <script>
-    $(document).ready(function() {
+        $(document).ready(function() {
 
-$('body').on("click", ".editbtn", function(event) {
+            $('body').on("click", ".editbtn", function(event) {
 
-    $('#editmodal').modal('show');
+                $('#editmodal').modal('show');
 
-    $tr = $(this).closest('tr');
+                $tr = $(this).closest('tr');
 
-    var data = $tr.children("td").map(function() {
-        return $(this).text();
-    }).get();
+                var data = $tr.children("td").map(function() {
+                    return $(this).text();
+                }).get();
 
-    console.log(data);
+                console.log(data);
 
-    $('#edit_id').val(data[0]);
-    $('#title').val(data[1]);
-    $('#introduction').val(data[2]);
-    $('#trend').val(data[3]);
-    $('#conclusion').val(data[4]);
+                $('#edit_id').val(data[0]);
+                $('#title').val(data[1]);
+                $('#introduction').val(data[2]);
+                $('#trend').val(data[3]);
+                $('#conclusion').val(data[4]);
 
-   
-   
-});
-});
- 
+
+
+            });
+        });
     </script>
     <script>
-$(document).ready(function(){
-    $('#addForm').on('submit', function(e){
-        e.preventDefault();
-        var formData = $('#addForm').serialize();
-        var requiredFields = ['title', 'introduction', 'trends','conclusion']; // List of required field names
-        var emptyFields = [];
-        requiredFields.forEach(function(field) {
-            if ($('[name="' + field + '"]').val() === '') {
-                emptyFields.push(field);
-            }
-        });
-        if (emptyFields.length > 0) {
-            Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: 'The following fields are required: ' + emptyFields.join(', ')
-            });
-            return;
-        }
-        $.ajax({
-            type: 'POST',
-            url: './includes/add.php',
-            data: formData,
-            success: function(response) {
-                Swal.fire({
-                    icon: 'success',
-                    title: 'Research Add Successfully',
-                    didClose: function() {
-                        // Refresh the page
-                        location.reload();
+        $(document).ready(function() {
+            $('#addForm').on('submit', function(e) {
+                e.preventDefault();
+                var formData = $('#addForm').serialize();
+                var requiredFields = ['title', 'introduction', 'trends', 'conclusion']; // List of required field names
+                var emptyFields = [];
+                requiredFields.forEach(function(field) {
+                    if ($('[name="' + field + '"]').val() === '') {
+                        emptyFields.push(field);
                     }
                 });
-                $('#modal').modal('hide');
-                // Add code here to update the table with the new data
-            }
+                if (emptyFields.length > 0) {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'The following fields are required: ' + emptyFields.join(', ')
+                    });
+                    return;
+                }
+                $.ajax({
+                    type: 'POST',
+                    url: './includes/add.php',
+                    data: formData,
+                    success: function(response) {
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Research Add Successfully',
+                            didClose: function() {
+                                // Refresh the page
+                                location.reload();
+                            }
+                        });
+                        $('#modal').modal('hide');
+                        // Add code here to update the table with the new data
+                    }
+                });
+            });
+
+
         });
-    });
- 
-
-});
-
     </script>
-<script>
-$(document).ready(function(){
-    $('#updateForm').on('submit', function(e){
-        e.preventDefault();
-        $.ajax({
-            type: 'POST',
-            url: './update.php',
-            data: $('#updateForm').serialize(),
-            success: function(response) {
-  Swal.fire({
-    icon: 'success',
-    title: 'Research Updated Successfully',
-    didClose: function() {
-            // Refresh the page
-            location.reload();
-        }
-  });
+    <script>
+        $(document).ready(function() {
+            $('#updateForm').on('submit', function(e) {
+                e.preventDefault();
+                $.ajax({
+                    type: 'POST',
+                    url: './update.php',
+                    data: $('#updateForm').serialize(),
+                    success: function(response) {
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Research Updated Successfully',
+                            didClose: function() {
+                                // Refresh the page
+                                location.reload();
+                            }
+                        });
 
-  $('#editmodal').modal('hide');
-  
-  // Add code here to update the table with the new data
+                        $('#editmodal').modal('hide');
 
-}
+                        // Add code here to update the table with the new data
+
+                    }
+                });
+
+            });
         });
-        
-    });
-});
-
     </script>
     <!--Archive researchs-->
     <script>
